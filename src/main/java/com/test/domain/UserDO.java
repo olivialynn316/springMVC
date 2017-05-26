4f78e449937c9c9be53504a5dc5375a74bfd9529
Kidding12345 <724432757@qq.com>
2017-05-26 11:04:31 +0800

    字符编码调整

--- src/main/java/com/test/domain/UserDO.java
+++ src/main/java/com/test/domain/UserDO.java
@@ -1,9 +1,16 @@
 package com.test.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author wb-yxj268701
  * @date 2017/05/22
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDO {
 
     /**
@@ -22,12 +29,6 @@ public class UserDO {
     private String password;
 
     /**
-     * 无参构造器
-     */
-    public UserDO() {
-    }
-
-    /**
      * 有参构造器,不含id
      *
      * @param username
@@ -35,43 +36,6 @@ public class UserDO {
      */
     public UserDO(String username, String password) {
         this.username = username;
-        this.password = password;
-    }
-
-    /**
-     * 完整有参构造器
-     *
-     * @param id
-     * @param username
-     * @param password
-     */
-    public UserDO(String id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
